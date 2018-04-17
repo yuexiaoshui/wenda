73c95330d842d78494b0d412c2dd8a5c931060a0
yue <yuexiaoshui@outlook.com>
2018-04-17 14:35:10 +0800

    vi

---
+++ src/main/java/com/vito/hello/web/HelloController.java
@@ -0,0 +1,17 @@
+package com.vito.hello.web;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+* @Classname: HelloController.java
+* @author: yue
+* @Time: 2018年4月12日--上午11:43:39
+* @Todo: TODO
+* @version V1.0 
+**/
+@RestController
+public class HelloController {
+	
+
+}
